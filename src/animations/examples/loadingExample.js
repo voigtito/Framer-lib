93772df822312ea3c4f964dcaeaad7668a3d8d0b
Gustavo Voigt <voigt.gustavoh@gmail.com>
2020-09-28 16:45:32 -0300

    Transforming loading into header

--- src/animations/examples/loadingExample.js
+++ src/animations/examples/loadingExample.js
@@ -1,68 +1,86 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 
 import "./styles.css";
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
   return (
     <div className="App">
-      <div className="loading">
-        <h3>Agenda is loading</h3>
-        <p>is loading ...</p>
-        <motion.div
-          initial={{ x: -100 }}
-          animate={{ x: 100 }}
-          transition={{
-            flip: Infinity,
-            duration: 1
-          }}
-          style={{
-            background: "var(--primary)",
-            height: 6,
-            width: 100
-          }}
-        ></motion.div>
-        <motion.div
-          initial={{ x: -180 }}
-          animate={{ x: 180 }}
-          transition={{
-            flip: Infinity,
-            duration: 1.2
-          }}
-          style={{
-            background: "var(--danger)",
-            height: 8,
-            width: 20
-          }}
-        ></motion.div>
-        <motion.div
-          initial={{ x: -180 }}
-          animate={{ x: 180 }}
-          transition={{
-            flip: Infinity,
-            duration: 1.5
-          }}
-          style={{
-            background: "var(--yellow)",
-            height: 4,
-            width: 200
-          }}
-        ></motion.div>
-      </div>
-      <main className="layout">
-        <h3>Grid Gallery</h3>
-        <div className="grid gallery">
-          {images.map((image, index) => (
-            <img
-              style={{
-                cursor: "pointer"
-              }}
-              src={`https://picsum.photos/seed/${image}/500/300`}
-              alt="placeholder"
-            />
-          ))}
-        </div>
-      </main>
+      <AnimateSharedLayout>
+        <motion.header layoutId="header" onClick={() => setIsLoading(true)}>
+          <motion.h1 layoutId="logo" className="fake-logo">
+            Agenda
+          </motion.h1>
+        </motion.header>
+        <main className="layout">
+          <h3>Grid Gallery</h3>
+          <AnimatePresence>
+            {isLoading && (
+              <motion.div
+                layoutId="header"
+                className="loading"
+                onClick={() => setIsLoading(false)}
+              >
+                <motion.h3 layoutId="logo">Agenda</motion.h3>
+                <motion.div exit={{ opacity: 0 }}>
+                  <p>is loading ...</p>
+                  <motion.div
+                    initial={{ x: -100 }}
+                    animate={{ x: 100 }}
+                    transition={{
+                      flip: Infinity,
+                      duration: 1
+                    }}
+                    style={{
+                      background: "var(--primary)",
+                      height: 6,
+                      width: 100
+                    }}
+                  />
+                  <motion.div
+                    initial={{ x: -180 }}
+                    animate={{ x: 180 }}
+                    transition={{
+                      flip: Infinity,
+                      duration: 1.2
+                    }}
+                    style={{
+                      background: "var(--danger)",
+                      height: 8,
+                      width: 20
+                    }}
+                  />
+                  <motion.div
+                    initial={{ x: -180 }}
+                    animate={{ x: 180 }}
+                    transition={{
+                      flip: Infinity,
+                      duration: 1.5
+                    }}
+                    style={{
+                      background: "var(--yellow)",
+                      height: 4,
+                      width: 200
+                    }}
+                  />
+                </motion.div>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <div className="grid gallery">
+            {images.map((image, index) => (
+              <img
+                style={{
+                  cursor: "pointer"
+                }}
+                src={`https://picsum.photos/seed/${image}/500/300`}
+                alt="placeholder"
+              />
+            ))}
+          </div>
+        </main>
+      </AnimateSharedLayout>
     </div>
   );
 }
